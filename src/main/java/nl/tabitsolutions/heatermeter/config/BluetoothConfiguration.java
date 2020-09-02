e9bdb6e08a62708e24d6d0ee0bb688d5ee25ed10
jochemvv <jochem.vanvroonhoven@trifork.nl>
2020-09-02 10:40:39 +0100

    initial code for BLE communication for actuators

--- src/main/java/nl/tabitsolutions/heatermeter/config/BluetoothConfiguration.java
+++ src/main/java/nl/tabitsolutions/heatermeter/config/BluetoothConfiguration.java
@@ -3,6 +3,7 @@ package nl.tabitsolutions.heatermeter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.sputnikdev.bluetooth.manager.impl.BluetoothManagerBuilder;
 import tinyb.BluetoothManager;
 
 @Configuration
@@ -11,6 +12,7 @@ public class BluetoothConfiguration {
 
     @Bean
     public BluetoothManager bluetoothManager() {
+        new BluetoothManagerBuilder().build(); // forces lib to be installed etc.
         return BluetoothManager.getBluetoothManager();
     }
 }
